585a20a4ef838a5596b363d94c036c1582fb7faa
Quentin SOFFIE <thibautmaringer0@gmail .com>
2019-02-14 09:49:02 +0100

    test

---
+++ test_utils.py
@@ -0,0 +1,21 @@
+# test_utils.py
+# Author: Thib et Quent
+# Version: February 8, 2018
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_fact(self):
+        self.assertEqual(utils.fact(5), 120)
+    
+    def test_roots(self):
+        self.assertEqual(utils.roots(1,2,1), (-1))
+    
+    def test_integrate(self):
+        self.assertEqual(utils.integrate((1, 0, 1), 1)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
