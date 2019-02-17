1d2f6f0c5c43c5fcc56bc01b51cf69b683e825b7
titimar16 <thibautmaringer0@gmail.com>
2019-02-17 15:59:10 +0000

    fix all

--- test_utils.py
+++ test_utils.py
@@ -1,31 +1,23 @@
-# test_utils.py
-# Author: Thib et Quent
-# Version: February 8, 2018
-
 import unittest
 import utils
 
 class TestUtils(unittest.TestCase):
-    def test_integrate(self):
-        def xcarre(x):
-            return x
-        self.assertEqual(utils.integrate(xcarre, 0, 1), 0.5)
-        
-
-
-
-
-   # def test_fact(self):
-    #    self.assertEqual(utils.fact(5), 120)
-      #  with self.assertRaises(ValueError): 
-       #     utils.fact(-5)
+    
+    def test_fact(self):
+        self.assertEqual(utils.fact(5), 120)
+        with self.assertRaises(ValueError): 
+            utils.fact(-5)
 
       
     
-  #  def test_roots(self):
-     #   self.assertEqual(utils.roots(1,2,1), (-1))
+    def test_roots(self):
+        self.assertEqual(utils.roots(1,-2,1), (1))
+        self.assertEqual(utils.roots(1,0,-1), (-1,1))
     
     
+    def test_integrate(self):
+        self.assertEqual(utils.integrate('x', 0, 1), 0.5)
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
--- utils.py
+++ utils.py
@@ -1,8 +1,3 @@
-# utils.py
-# Math library
-# Author: Sébastien Combéfis
-# Version: February 8, 2018
-
 from math import *
 from scipy.integrate import quad
 
@@ -20,13 +15,13 @@ def fact(n):
 
 def roots(a, b, c):
 
-	z = (b^2 - 4*a*c)
+	z = (b**2 - 4*a*c)
 	if z == 0:
 		g = -b/(2*a)
 		return (g)
 	elif z > 0:
-		g = (-b + sqrt(z))/(2*a)
-		h = (-b - sqrt(z))/(2*a)
+		g = (-b - sqrt(z))/(2*a)
+		h = (-b + sqrt(z))/(2*a)
 		return (g, h)
 	else:
 		return None
@@ -34,23 +29,10 @@ def roots(a, b, c):
 
 
 def integrate(function, lower, upper):
-	"""Approximates the integral of a fonction between two bounds
-	
-	Pre: 'function' is a valid Python expression with x as a variable,
-		'lower' <= 'upper',
-		'function' continuous and integrable between 'lower‘ and 'upper'.
-	Post: Returns an approximation of the integral from 'lower' to 'upper'
-		of the specified 'function'.
-
-	Hint: You can use the 'integrate' function of the module 'scipy' and
-		you'll probably need the 'eval' function to evaluate the function
-		to integrate given as a string.
-	
-	"""
 	if lower <= upper:
-		f= lambda x:function
-		i = scipy.integrate.quad(f, lower, upper)
-		return i
+		f= lambda x:eval(function)
+		i = quad(f, lower, upper)
+		return i[0]
 	
 	    
 
@@ -61,3 +43,5 @@ if __name__ == '__main__':
 	print(fact(5))
 	print(roots(1, 0, 1))
 	print(integrate('x ** 2 - 1', -1, 1))
+
+print(integrate('x ** 2 - 1', -1, 1))
